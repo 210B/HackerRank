77eaf0555089d777c8478d7e361008e454dac915
210B <leah8735@gmail.com>
2023-11-01 09:24:30 +0900

    Update Time_Conversion.py

--- solution/Time_Conversion.py
+++ solution/Time_Conversion.py
@@ -36,3 +36,44 @@ Sample Output
 
 19:05:45
 '''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    hour, minute, second_AMPM = s.split(':')
+
+    if second_AMPM[2:] == "PM" and hour != '12':
+        hour = str(int(hour) + 12)
+    if second_AMPM[2:] == "AM" and hour == '12':
+        hour = "00"
+    if second_AMPM[2:] == "PM" and hour == '12':
+        hour = "12"
+
+    conversion_time = hour + ':' + minute + ':' + second_AMPM[0:2]
+
+    return conversion_time
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
